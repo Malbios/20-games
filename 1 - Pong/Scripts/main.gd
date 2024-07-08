97d4888301f150f769953718fe3e2892ab22019d
Malbios <no@mail>
2024-07-08 15:21:48 +0200

    .

--- 1 - Pong/Scripts/main.gd
+++ 1 - Pong/Scripts/main.gd
@@ -1,31 +1,116 @@
 extends Node
 
-@export var player_gadget: CharacterBody2D
-@export var bot_gadget: CharacterBody2D
-@export var ball_gadget: CharacterBody2D
+@export var ball_speed = 400
 
-var round_started = false
+@export var player: CharacterBody2D
+@export var bot: CharacterBody2D
+@export var player_score: Label
+@export var bot_score: Label
 
-# Called when the node enters the scene tree for the first time.
+var player_score_value := 0
+var bot_score_value := 0
+
+var ball_prefab := preload("res://Scenes/ball.tscn")
+var bounce_sound := preload("res://Assets/bounce.mp3")
+var player_scored_sound := preload("res://Assets/player_scored.mp3")
+var bot_scored_sound := preload("res://Assets/player_scored.mp3")
+
+var bounce_sound_player := AudioStreamPlayer.new()
+var player_scored_sound_player := AudioStreamPlayer.new()
+var bot_scored_sound_player := AudioStreamPlayer.new()
+
+var ball: RigidBody2D
+
+var initial_player_position
+var initial_bot_position
+
+var running_match := false
+
+
+func set_elements_process(enabled: bool):
+	player.set_process(enabled)
+	player.set_physics_process(enabled)
+	bot.set_process(enabled)
+	bot.set_physics_process(enabled)
+
+
+func reset_ball():
+	if is_instance_valid(ball):
+		ball.queue_free()
+
+	ball = ball_prefab.instantiate()
+	ball.contact_monitor = true
+	ball.max_contacts_reported = 1
+	ball.body_entered.connect(handle_ball_collision)
+	add_child(ball)
+	var force_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	force_vector *= ball_speed
+	ball.apply_impulse(force_vector)
+
+
+func set_game_elements(enable: bool):
+	if enable:
+		$StartHint.hide()
+		set_elements_process(true)
+		player_score_value = 0
+		bot_score_value = 0
+		reset_ball()
+	else:
+		if ball:
+			ball.queue_free()
+		player.position = initial_player_position
+		player.velocity = Vector2.ZERO
+		bot.position = initial_bot_position
+		bot.velocity = Vector2.ZERO
+		set_elements_process(false)
+		$StartHint.show()
+
+	running_match = enable
+
+
+func handle_ball_collision(body: Node):
+	if body.name == "WorldBoundaryRight":
+		player_scored_sound_player.play()
+		player_score_value += 1
+		player_score.text = str(player_score_value)
+		reset_ball()
+	elif body.name == "WorldBoundaryLeft":
+		bot_scored_sound_player.play()
+		bot_score_value += 1
+		bot_score.text = str(bot_score_value)
+		reset_ball()
+	else:
+		bounce_sound_player.play()
+
+
 func _ready():
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_RESIZE_DISABLED, true)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-	player_gadget.set_process(false)
-	bot_gadget.set_process(false)
-	ball_gadget.set_process(false)
+	bounce_sound_player.stream = bounce_sound
+	add_child(bounce_sound_player)
+	player_scored_sound_player.stream = player_scored_sound
+	add_child(player_scored_sound_player)
+	bot_scored_sound_player.stream = bot_scored_sound
+	add_child(bot_scored_sound_player)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_released("ui_cancel"):
+	initial_player_position = player.position
+	initial_bot_position = bot.position
+
+	set_game_elements(false)
+
+
+func _input(_event):
+	if Input.is_action_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+	if Input.is_action_just_released("quit"):
 		get_tree().quit()
 
-	if not round_started and Input.is_action_just_released("restart"):
-		player_gadget.set_process(true)
-		bot_gadget.set_process(true)
-		ball_gadget.set_process(true)
-
-		ball_gadget.show()
-		$StartHint.hide()
-
-		round_started = true
+	if not running_match and Input.is_action_just_released("restart"):
+		set_game_elements(true)
+	elif running_match and Input.is_action_just_released("restart"):
+		set_game_elements(false)
--- 1 - Pong/Scripts/player.gd
+++ 1 - Pong/Scripts/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+var speed: float = 800.0
+var acceleration: float = 2.0
+
 
 func _physics_process(delta: float):
-	if Input.is_action_pressed(("up")):
-		velocity.y -= SPEED * delta
-
-	if Input.is_action_pressed("down"):
-		velocity.y += SPEED * delta
+	if Input.is_action_pressed("break"):
+		velocity = Vector2.ZERO
+	else:
+		var mouse_velocity = Input.get_last_mouse_velocity().normalized()
+		velocity = lerp(velocity, Vector2(0, mouse_velocity.y) * speed, delta * acceleration)
 
 	move_and_slide()
